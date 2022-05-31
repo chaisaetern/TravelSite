8da2af33221584f22ecfcddc3325c766bdcd2d5c
Chai <csaetern@alphaworks.tech>
2022-05-31 11:10:35 -0700

    about commit

--- page-about.php
+++ page-about.php
@@ -20,4 +20,27 @@
 </main>
 <!-- This is the end of hero section -->
 
+<!-- This is the start of banner section -->
+<!-- Daily quotes? generated everyday 12||24hrs -->
+<div class="banner-about">
+    <section class="banner-about-section">
+        <h2>"100% of success equals a fraction of fearing failure."</h2>
+        <small>- Unknown Author</small>
+    </section>
+</div>
+<!-- This is the end of banner section -->
+
+<!-- This is the start of body about section -->
+<div class="body-about-container">
+    <div class="body-about-top">
+        <div class="body-about-top-left"></div>
+        <div class="body-about-top-right"></div>
+    </div>
+    <div class="body-about-bottom">
+        <div class="body-about-bottom-left"></div>
+        <div class="body-about-bottom-right"></div>
+    </div>
+</div>
+<!-- This is the end of body about section -->
+
 <?php get_footer(); ?>
